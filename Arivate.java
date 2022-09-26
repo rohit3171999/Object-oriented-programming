7b5619241848c2bdfda9dba8d360fbdc66717a2b
Rohit Sharma <rohit3171999@gmail.com>
2022-09-26 13:23:02 +0530

    private	The code is only accessible within the declared class

---
+++ Arivate.java
@@ -0,0 +1,14 @@
+public class Arivate {
+    private String fname = "rohit";
+    private String lname = "sharma";
+    private String email = "rohit.sharma1_cs19@gla.ac.in";
+    private int age = 22;
+
+    public static void main(String[] args) {
+        Arivate myObj = new Arivate();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+    }
+
+}
