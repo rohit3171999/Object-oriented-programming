55a52776b05cc4e30709c0861134ba151a0b9be0
Rohit Sharma <rohit3171999@gmail.com>
2022-09-26 13:56:10 +0530

    Encapsulation Getter & Setter

---
+++ Encap.java
@@ -0,0 +1,11 @@
+public class Encap {
+    private String name;
+    //Getter
+    public String getName(){
+        return name;
+    }
+    //Setter
+    public void setName(String newName){
+        this.name=newName;
+    }
+}
---
+++ Encap2.java
@@ -0,0 +1,8 @@
+public class Encap2 {
+    public static void main(String[] args) {
+        Encap myObj=new Encap();
+        //myObj.name="rohit";
+        myObj.setName("sharma");
+        System.out.println(myObj.getName());
+    }
+}
