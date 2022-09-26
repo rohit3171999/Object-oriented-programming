afb4bbabd9294972d581323910723d7fb2c00ae9
Rohit Sharma <rohit3171999@gmail.com>
2022-09-26 14:12:05 +0530

    one more encapsulation example

---
+++ Encap3.java
@@ -0,0 +1,10 @@
+public class Encap3 {
+    private String address;
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String newAddress){
+        this.address = newAddress;
+    }
+    
+}
---
+++ Encap4.java
@@ -0,0 +1,9 @@
+public class Encap4 {
+    public static void main(String[] args) {
+        Encap3 newObj=new Encap3();
+        //newObj.address="ghaziabad";
+        newObj.setAddress("ghaziabad");
+        System.out.println(newObj.getAddress());
+    }
+    
+}
