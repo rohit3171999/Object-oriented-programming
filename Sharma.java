59d0e526abb8b316bb4198d9c1cd673dbc088672
Rohit Sharma <rohit3171999@gmail.com>
2022-09-26 13:13:19 +0530

    modifiers concept

---
+++ Sharma.java
@@ -0,0 +1,9 @@
+public class Sharma {
+    public static void main(String[] args) {
+        Rohit myObj = new Rohit(); 
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+      }
+    
+}
