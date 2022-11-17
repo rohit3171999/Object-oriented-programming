a24e13beca8037280e520b365a53ad46184d8029
Rohit Sharma <rohit3171999@gmail.com>
2022-11-17 13:19:34 +0530

    temp

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main1{
+    static void myMethod(String name){
+        //System.out.println("hello rohit");
+        System.out.println("Welcome "+name);
+    }
+    public static void main(String[] args) {
+        myMethod("rohit");
+        // myMethod();
+        // myMethod();
+    }
+}
