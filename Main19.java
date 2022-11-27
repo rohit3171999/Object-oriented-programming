81ec6b0e8512a6de6b381d8a7beee73d8189f510
Rohit Sharma <rohit3171999@gmail.com>
2022-11-27 15:12:18 +0530

    Scanner Class

---
+++ Main19.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main19 {
+    public static void main(String[] args) {
+        Scanner myObj= new Scanner(System.in);
+        System.out.println("enter username");
+        String username=myObj.nextLine();
+        System.out.println("Username is "+username);
+
+    }
+}
