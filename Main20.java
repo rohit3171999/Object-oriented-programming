927798bfe8de0edfc0e173e118fc3486994710ef
Rohit Sharma <rohit3171999@gmail.com>
2022-11-27 21:29:27 +0530

    One Extra Scanner Class Example

---
+++ Main20.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main20 {
+    public static void main(String[] args) {
+        Scanner myObject=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String username=myObject.nextLine();
+        System.out.println("The String is "+username);
+    }
+}
